7e7fb227592b16223c98c5e94b69db3aea7f90f9
Muhammad Muttraf Rasool <muttrafgujjar.787@gmail.com>
2022-12-10 03:53:45 +0500

    Feature: ES6 modules syntax

--- main.js
+++ main.js
@@ -1,41 +1,40 @@
 /* eslint-disable class-methods-use-this */
 /* eslint-disable no-unused-vars */
+import Book from './modules/Book.js';
+
+const booksList = document.getElementById('booksList');
+const newBook = document.getElementById('newBook');
+const contactInfo = document.getElementById('contactInfo');
+const recordSec = document.querySelector('.record-section');
+const addForm = document.querySelector('.form');
+const contactSec = document.querySelector('.contact');
 const showBooks = document.querySelector('.record');
 const Title = document.querySelector('.title');
 const Author = document.querySelector('.author');
 const btn = document.querySelector('.form button');
 
-class Book {
-  constructor() {
-    this.books = [];
-  }
-
-  addBook(obj) {
-    this.books = JSON.parse(localStorage.getItem('data'));
-    if (this.books === null) {
-      this.books = [];
-      localStorage.setItem('data', this.books);
-      this.books.push(obj);
-      localStorage.setItem('data', JSON.stringify(this.books));
-      window.location.reload();
-    } else {
-      this.books.push(obj);
-      localStorage.setItem('data', JSON.stringify(this.books));
-      window.location.reload();
-    }
-  }
-
-  removeBook(id) {
-    this.books = JSON.parse(localStorage.getItem('data'));
-    this.books = this.books.filter((i) => i?.id !== id);
-    localStorage.setItem('data', JSON.stringify(this.books));
-    window.location.reload();
-  }
-}
-
 const bookc = new Book();
 
-const newBook = () => {
+booksList.addEventListener('click', () => {
+  window.location.reload();
+  recordSec.style.display = 'block';
+  addForm.style.display = 'none';
+  contactSec.style.display = 'none';
+});
+
+newBook.addEventListener('click', () => {
+  recordSec.style.display = 'none';
+  addForm.style.display = 'block';
+  contactSec.style.display = 'none';
+});
+
+contactInfo.addEventListener('click', () => {
+  recordSec.style.display = 'none';
+  addForm.style.display = 'none';
+  contactSec.style.display = 'block';
+});
+
+btn.addEventListener('click', () => {
   if (Title.value === '' || Author.value === '') {
     document.querySelector('.form > span').textContent = 'All fiels are required!';
   } else {
@@ -46,22 +45,27 @@ const newBook = () => {
     };
     bookc.addBook(bookObj);
   }
-};
+});
 
 // ***********
+
+window.remove = (id) => {
+  bookc.removeBook(id);
+};
 
 // Locale storage
 
 window.addEventListener('DOMContentLoaded', () => {
+  recordSec.style.display = 'block';
+  addForm.style.display = 'none';
+  contactSec.style.display = 'none';
   const books = JSON.parse(localStorage.getItem('data'));
-  showBooks.innerHTML = books
-    .map(
-      (i) => `
+  books.forEach((i) => {
+    showBooks.innerHTML += `
       <div class="element">
       <h5>${i?.title} by ${i?.author} </h5>
-      <button onClick='bookc.removeBook(${i?.id})'>Remove</button>
+      <button type='button' onClick='remove(${i?.id})'>Remove</button>
       </div>
-      `,
-    )
-    .join('');
+      `;
+  });
 });
